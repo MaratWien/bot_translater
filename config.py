c60b1e67984010e71a3505ffb5de8fa0b6b05464
Марат Шамсутдинов <hfunctio@student.21-school.ru>
2023-01-30 15:00:21 +0300

    second

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 secret = {
-    'token': 'MTA2Mjc2NjU1MzI2NDgzMjU1Mg.GLReMj.JVMy7bqXFH0bGdk5hxDdSN1NZOvhMy-9ePhJ-A',
+    'token': '',
     'prefix': '_',
     'cl_id': '1031558752173826168',
     'perms': 8
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import config
 import discord
+import data_objects
 import googletrans
 from googletrans import Translator
 
@@ -18,35 +19,40 @@ class IDError(Exception):
 
 @client.event
 async def on_ready():
-    print('Авторизован как: ' + str(client.user) +'.\nЧтобы пригласить бота на сервер используйте ссылку: \nhttps://discordapp.com/oauth2/authorize?&client_id=' + config.secret['cl_id'] + '&scope=bot&permissions=' + str(config.secret['perms']))
+    print('Авторизован как: ' +str(client.user) +'.\nЧтобы пригласить бота на сервер используйте ссылку: \nhttps://discordapp.com/oauth2/authorize?&client_id=' +config.secret['cl_id'] +'&scope=bot&permissions=' +str(config.secret['perms']))
 
 
 @client.event
 async def on_message(message: discord.Message):
     msg_text: str = message.content
-
     try:
         if message.author.id == 1031558752173826168:
             raise IDError
         else:
+            writer: data_objects.User = data_objects.User(message.author.id)
             if msg_text.startswith('.tr_mode'):
                 command = msg_text.split(' ')
                 try:
                     lang = command[1]
                     if lang in langs:
                         await message.channel.send(f'Переводчик включён для языка: {lang}')
+                        writer.update({'tr_lang': lang})
                     else:
                         await message.channel.send(f'Язык {lang} недоступен для перевода!')
                 except IndexError:
                     await message.channel.send('Переводчик отключен!')
+                    writer.update({'tr_lang': ''})
             else:
-                if lang:
-                    translated = tr.translate(msg_text, dest=lang)
-                    if translated.src != lang:
+
+                w_lang = writer.user['tr_lang']
+
+                if w_lang != '':
+                    translated = tr.translate(msg_text, dest=writer.user['tr_lang'])
+                    if translated.src != writer.user['tr_lang']:
                         await message.channel.send(translated.text)
 
-    except IDError as ex:
-        await message.channel.send(ex)
+    except IDError:
+        pass
 
 
 client.run(config.secret['token'])
